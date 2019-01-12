2553e516c0bec97ebec2199ecf744fcd72c1d9d4
Hooman007 <hmn16@yahoo.com>
2019-01-11 21:13:16 -0800

    add both fitbit api credentials

--- FITBIT_Data_Collection_Interface/DataExtraction.py
+++ FITBIT_Data_Collection_Interface/DataExtraction.py
@@ -2,16 +2,21 @@ import fitbit
 import gather_keys_oauth2 as Oauth2
 import pandas as pd
 import datetime
-CLIENT_ID ='22DF24'
-CLIENT_SECRET = '7848281e9151008de32698f7dd304c68'
-# Note: This ID belong's to the fitbit under 'UBC_MIST' (Not Hooman's)
+
+# ************ UBC MIST Account *********
+# CLIENT_ID ='22DF24'
+# CLIENT_SECRET = '7848281e9151008de32698f7dd304c68'
+
+# ************ Hooman's Account *********
+CLIENT_ID ='22D68G'
+CLIENT_SECRET = '32e28a7e72842298fd5d97ce123104ca'
+
 
 server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
 server.browser_authorize()
 ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
 REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
 auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
-#access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
 
 yesterday = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d"))   # To avoid updating dates everyday
 yesterday2 = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
