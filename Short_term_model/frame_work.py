43a9c598f56247f37c9f6178156e4dfc0af55e90
Hooman007 <hmn16@yahoo.com>
2019-03-16 11:19:39 -0700

    priliminary model training script

--- Short_term_model/frame_work.py
+++ Short_term_model/frame_work.py
@@ -18,10 +18,11 @@ from scipy.signal import resample
 import matplotlib.pyplot as plt
 import pickle
 from sklearn.preprocessing import OneHotEncoder
+import keras
 from keras.models import Model
 from keras.layers import Input, Dense, Conv1D, MaxPooling1D, Softmax, Add, Flatten, Activation# , Dropout
 from keras import backend as K
-from keras.optimizers import Adam
+from keras.optimizers import Adam, sgd
 from keras.callbacks import LearningRateScheduler, ModelCheckpoint
 import os
 
@@ -39,11 +40,20 @@ def GetTimeRangeSampledDataFrame(df_x, time_y, sample_time_range, sampling_metho
         raise ("sampling_method not recognized")
     return df_x[(df_x.Time > start_time) & (df_x.Time < end_time)]
 
+def plot_model_network(model, dst):
+    print(model.summary())
+    from keras.utils import plot_model
+    plot_model(model, show_layer_names=True, show_shapes=True, to_file=dst)
+
 # ********************************************************************
 # ********************* Start of the script **************************
 # ********************************************************************
 # region HYPER-PARAMETERS
+np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
+
 # region Model hyper parameters
+BATCH_SIZE = 50
+LEARNING_RATE = 0.001
 SELECTED_ROWS_IN_SAMPLING = 50   # same as time dimension of model
 # endregion Model hyper parameters
 
@@ -146,5 +156,131 @@ print("input heart rate data points shape is: {}" .format(all_data_dict['train']
 print("output anxiety level data points shape is: {}" .format(all_data_dict['train']['labels'].shape))
 #endregion creating the input and output database from the csv files
 
+#region deleting the dataframes
+del df_x_sampled
+del selected_dataFrame
+del df_x
+del df_y
+#endregion deleting the dataframes
 
+# region showing the data label distribution
+train_label_df =  pd.DataFrame(data=all_data_dict.get('train').get('labels'),columns=['Anxiety_Level'])
+print(train_label_df.Anxiety_Level.value_counts())
+del train_label_df
+# endregion showing the data label distribution
 # endregion
+
+y_train = all_data_dict.get('train').get('labels')
+x_train = all_data_dict.get('train').get('Data')
+x_train = np.expand_dims(x_train, 2)
+
+print('=' * 80)
+print('Start Training')
+print("[INFO] training network...")
+
+# region make and compile model and optimizer
+# region Model Architecture
+K.clear_session()
+
+# ************** preparation for Keras layers ****************
+K.set_image_data_format('channels_last')  # to make sure the input shape is [ hr time, channel]
+
+n_obs, time, depth = x_train.shape
+batch_size = 500
+
+input_tensor = Input(shape=(time, depth),name='input_HR')
+kernel_init = "he_uniform"
+kernel_regul = keras.regularizers.l2(l=1e-4)
+
+C11 = Conv1D(filters=32, kernel_size=5, padding='valid', strides=1,
+           activation='linear', kernel_initializer=kernel_init, use_bias=False,
+           kernel_regularizer=kernel_regul)(input_tensor)
+
+C12 = Conv1D(filters=32, kernel_size=5, padding='same', strides=1,
+           activation='relu', kernel_initializer=kernel_init, use_bias=False,
+           kernel_regularizer=kernel_regul)(C11)
+C13 = Conv1D(filters=32, kernel_size=5, padding='same', strides=1,
+           activation='linear', kernel_initializer=kernel_init, use_bias=False,
+           kernel_regularizer=kernel_regul)(C12)
+S11 = Add()([C13, C11])
+A12 = Activation("relu")(S11)
+M11 = MaxPooling1D(pool_size=5, strides=2)(A12)
+
+C21 = Conv1D(filters=32, kernel_size=5, padding='same', strides=1,
+           activation='relu', kernel_initializer=kernel_init, use_bias=False,
+           kernel_regularizer=kernel_regul)(M11)
+C22 = Conv1D(filters=32, kernel_size=5, padding='same', strides=1,
+           activation='linear', kernel_initializer=kernel_init, use_bias=False,
+           kernel_regularizer=kernel_regul)(C21)
+S21 = Add()([C22, M11])
+A22 = Activation("relu")(S21)
+M21 = MaxPooling1D(pool_size=5, strides=2)(A22)
+
+# C31 = Conv1D(filters=32, kernel_size=5, padding='same', strides=1,
+#            activation='relu', kernel_initializer=kernel_init, use_bias=False,
+#            kernel_regularizer=kernel_regul)(M21)
+# C32 = Conv1D(filters=32, kernel_size=5, padding='same', strides=1,
+#            activation='linear', kernel_initializer=kernel_init, use_bias=False,
+#            kernel_regularizer=kernel_regul)(C31)
+# S31 = Add()([C32, M21])
+# A32 = Activation("relu")(S31)
+# M31 = MaxPooling1D(pool_size=5, strides=2)(A32)
+#
+# C41 = Conv1D(filters=32, kernel_size=5, padding='same', strides=1,
+#            activation='relu', kernel_initializer=kernel_init, use_bias=False,
+#            kernel_regularizer=kernel_regul)(M31)
+# C42 = Conv1D(filters=32, kernel_size=5, padding='same', strides=1,
+#            activation='linear', kernel_initializer=kernel_init, use_bias=False,
+#            kernel_regularizer=kernel_regul)(C41)
+# S41 = Add()([C42, M31])
+# A42 = Activation("relu")(S41)
+# M41 = MaxPooling1D(pool_size=5, strides=2)(A42)
+#
+# C51 = Conv1D(filters=32, kernel_size=5, padding='same', strides=1,
+#            activation='relu', kernel_initializer=kernel_init, use_bias=False,
+#            kernel_regularizer=kernel_regul)(M41)
+# C52 = Conv1D(filters=32, kernel_size=5, padding='same', strides=1,
+#            activation='linear', kernel_initializer=kernel_init, use_bias=False,
+#            kernel_regularizer=kernel_regul)(C51)
+# S51 = Add()([C52, M41])
+# A52 = Activation("relu")(S51)
+# M51 = MaxPooling1D(pool_size=5, strides=2)(A52)
+
+F1 = Flatten()(M21)
+
+D1 = Dense(units=32, activation='relu')(F1)
+D2 = Dense(units=32, activation='relu')(D1)
+output_tensor = Dense(units=1, activation='sigmoid')(D2)
+
+model = Model(inputs=input_tensor, outputs=output_tensor)
+print(model.summary())
+# plot_model_network(model=model, dst=os.path.join('model_keras.png'))# TODO Fix graphviz stuff
+# endregion Model Architecture
+
+# opt = Adam(lr = 0.001, beta_1 = 0.9, beta_2 = 0.999)
+opt = sgd(lr = LEARNING_RATE, decay=0.825)
+
+# opt = sgd(lr = self.meta_data['base_lr'],
+#                           decay=self.meta_data['decay'],
+#                           momentum = self.meta_data['momentum'],
+#                           nesterov=self.meta_data['nesterov_bool'])
+
+model.compile(optimizer=opt # TODO try the model with "categorical_crossentropy" as well
+              , loss='mean_squared_error', metrics=['accuracy'])
+# , loss=self.meta_data['loss'],
+# loss_weights=self.meta_data['loss_weights'],
+# metrics=self.meta_data['metric'])
+# endregion make and compile model and optimizer
+
+H = model.fit(
+    x = x_train, y = y_train[:,0], batch_size=BATCH_SIZE, epochs=100, verbose=2,
+    validation_split=0.2, shuffle=True,
+    # class_weight=None, #TODO use this
+    # sample_weight=None, #TODO use this maybe
+    # initial_epoch=0,
+    # steps_per_epoch=None,
+    # validation_steps=None,
+    # validation_freq=1
+    )
+
+print("End of Training Epoch\n", "-" * 80)
