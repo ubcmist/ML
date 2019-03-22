b00f785d923196233732ca6574b3ef5c3a4474a8
Hooman007 <hmn16@yahoo.com>
2019-03-21 21:34:12 -0700

    add arduino GSR Logger for data collection

---
+++ Python_Arduino_Interface/Arduino_Gsr_Logger/Arduino_Gsr_Logger.ino
@@ -0,0 +1,19 @@
+const int GSR=A0;
+int sensorValue=0;
+int gsr_average=0;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  long sum=0;
+  for(int i=0;i<50;i++)           //Average the 50 measurements to remove the glitch
+      {
+      sensorValue=analogRead(GSR);
+      sum += sensorValue;
+      delay(5);
+      }
+   gsr_average = sum/50;
+   Serial.println(gsr_average);
+}
