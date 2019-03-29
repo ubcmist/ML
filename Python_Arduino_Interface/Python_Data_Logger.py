2c8e9070332203c2aabc4ef9026810664451f895
Hooman007 <hmn16@yahoo.com>
2019-03-29 15:18:39 -0700

    nit add citation

--- Python_Arduino_Interface/Python_Data_Logger.py
+++ Python_Arduino_Interface/Python_Data_Logger.py
@@ -1,3 +1,8 @@
+'''
+This script is inspired by:
+https://engineersportal.com/blog/2018/2/25/python-datalogger-reading-the-serial-output-from-arduino-to-analyze-data-using-pyserial 
+'''
+
 import serial
 import time
 import csv
