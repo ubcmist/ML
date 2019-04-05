8ecd45aa5ef31a1fed38156cd316d6b50c3570c1
Hooman007 <hmn16@yahoo.com>
2019-04-05 13:31:09 -0700

    hr upsampling now outputs rounded and not rounded

--- Python_Arduino_Interface/HR_Upsampling_1S.py
+++ Python_Arduino_Interface/HR_Upsampling_1S.py
@@ -24,24 +24,27 @@ parser.add_argument('-o', '--output', type=str, default=None, help='address of t
 parser.add_argument('-t', '--type', type=str, default='linear', help='up-sampling interpolation type: (linear/spline). Linear is best option')
 parser.add_argument('-d', '--date', type=str, default=None, help='Date data was taken. format: yyyy-mm-dd')
 args = vars(parser.parse_args())
-Input_GSR_csv = args['input']
-output_GSR_csv = args['output']
+Input_HR_csv = args['input']
+output_HR_csv = args['output']
 resampling_type = args['type']
 collection_Date = args['date']
-# Input_GSR_csv = 'Data/VideoGames_Apex_HR_GSR_Hooman/HR/20190322_x.csv'
-# output_GSR_csv = 'Data/VideoGames_Apex_HR_GSR_Hooman/HR_UpSampled_1S/20190322_x.csv'
+# Input_HR_csv = 'Data/VideoGames_Apex_HR_GSR_Hooman/HR/20190322_x.csv'
+# output_HR_csv = 'Data/VideoGames_Apex_HR_GSR_Hooman/HR_UpSampled_1S/20190322_x.csv'
 # resampling_type = "linear"
 # collection_Date = '2019-03-22'
 
 
 def date_time_parser(x):
-	return datetime.strptime(collection_Date+ '-' +x, '%Y-%m-%d-%H:%M:%S')
+    if collection_Date is None:
+        return datetime.strptime(x, '%Y-%m-%d-%H:%M:%S')
+    else:
+        return datetime.strptime(collection_Date+ '-' +x, '%Y-%m-%d-%H:%M:%S')
 
-HR_original = read_csv(Input_GSR_csv, header=0, parse_dates=[0], index_col=0, squeeze=True,  date_parser=date_time_parser)
+HR_original = read_csv(Input_HR_csv, header=0, parse_dates=[0], index_col=0, squeeze=True,  date_parser=date_time_parser)
 resampled = HR_original.resample('1S')
 if resampling_type == "linear":
     interpolated = resampled.interpolate(method='linear')
-    interpolated = interpolated.round()
+    rounded = interpolated.round()
 elif resampling_type == "spline":
     interpolated = resampled.interpolate(method='spline', order=2)
 
@@ -49,9 +52,12 @@ if len(interpolated[interpolated.isnull() == True]) != 0:
     print("WARNING: there are rows that could not be resampled due to not having enough data:")
     print(interpolated[interpolated.isnull() == True])
 
-if output_GSR_csv is None:
-    output_GSR_csv = osp.join(osp.dirname(Input_GSR_csv) + '_UpSampled_1S', osp.basename(Input_GSR_csv))
-
-if not osp.exists(osp.dirname(output_GSR_csv)):
-    os.makedirs(osp.dirname(output_GSR_csv))
-interpolated.to_csv(output_GSR_csv[:-4] + "_" + resampling_type +'.csv' , header =True, date_format='%Y-%m-%d-%H:%M:%S')
+if output_HR_csv is None:
+    output_HR_csv_rounded = osp.join(osp.dirname(Input_HR_csv) + '_UpSampled_1S', osp.basename(Input_HR_csv))
+    output_HR_csv_NotRounded = osp.join(osp.dirname(Input_HR_csv) + '_UpSampled_1S_NotRounded', osp.basename(Input_HR_csv))
+if not osp.exists(osp.dirname(output_HR_csv_rounded)):
+    os.makedirs(osp.dirname(output_HR_csv_rounded))
+rounded.to_csv(output_HR_csv_rounded[:-4] + "_" + resampling_type +'.csv' , header =True, date_format='%Y-%m-%d-%H:%M:%S')
+if not osp.exists(osp.dirname(output_HR_csv_NotRounded)):
+    os.makedirs(osp.dirname(output_HR_csv_NotRounded))
+interpolated.to_csv(output_HR_csv_NotRounded[:-4] + "_" + resampling_type +'.csv' , header =True, date_format='%Y-%m-%d-%H:%M:%S')
